21ccde697829b19f7699a8c1622fb0d953b0c4b6
diamant <diamant.alexandre@gmail.com>
2023-03-31 09:41:00 +0200

    test

--- test/client_test.c
+++ test/client_test.c
@@ -6,9 +6,12 @@
 #include <unistd.h>
 
 #define SIZE_MESS 100
+#define MAX_USERNAME_LEN 10
+
+#define BUF_LEN 26 // 16 bytes for header + 10 bytes for username
 
 int main(int argc, char *argv[]) {
-    int sock = socket(PF_INET6, SOCK_DGRAM, 0);
+    /*int sock = socket(PF_INET6, SOCK_DGRAM, 0);
     if (sock < 0) return -1;
 
     // adresse de destination
@@ -21,7 +24,9 @@ int main(int argc, char *argv[]) {
 
     char buffer[SIZE_MESS];
     memset(buffer, 0, SIZE_MESS);
-    sprintf(buffer, "TEST");
+    // 0 : inscription
+    // 1 : connexion
+    fgets(buffer, SIZE_MESS, stdin);
 
     int r = sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr *) &servadr, len);
     if (r < 0) {
@@ -37,5 +42,83 @@ int main(int argc, char *argv[]) {
     }
     printf("message recu - %d octets : %s\n", r, buffer);
 
+    // ENVOIE DU PSEUDO
+    uint16_t id = 0;
+    uint8_t type = 1;
+    uint16_t header_client = htons((id << 5) | (type & 0x1F));
+    r = sendto(sock, &header_client, sizeof(header_client), 0, (struct sockaddr *) &servadr, len);
+    memset(buffer, 0, SIZE_MESS);
+    fgets(buffer, MAX_USERNAME_LEN, stdin);
+    r = sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr *) &servadr, len);
+
+    // RECEPTION DE L'ID
+    uint16_t header_server;
+    r = recvfrom(sock, &header_server, sizeof(header_server), 0, (struct sockaddr *) &servadr, &len);
+    if (r < 0) {
+        perror("recvfrom");
+        return -1;
+    }
+    id = ntohs(header_server) >> 5;
+    type = ntohs(header_server) & 0x1F;
+
+    printf("id : %d, type : %d\n", id, type);
+
+    close(sock);*/
+
+    int sock = socket(PF_INET6, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        perror("Erreur socket");
+        return -1;
+    }
+
+    const char *ip = argv[1];
+    int port = atoi(argv[2]);
+
+    struct sockaddr_in6 servadr;
+    memset(&servadr, 0, sizeof(servadr));
+    servadr.sin6_family = AF_INET6;
+    inet_pton(AF_INET6, ip, &servadr.sin6_addr);
+    servadr.sin6_port = htons(port);
+    socklen_t len = sizeof(servadr);
+
+    char buffer[BUF_LEN];
+    
+    //entete
+    uint16_t header;
+    uint16_t id = 0;
+    uint8_t type = 1;
+    header = htons((id << 5) | (type & 0x1F));
+
+    // pseudo
+    char username[MAX_USERNAME_LEN+1];
+    memset(username, 0, MAX_USERNAME_LEN+1);
+    fgets(username, MAX_USERNAME_LEN, stdin);
+
+    // copie de l'entete et du pseudo dans le buffer
+    memcpy(buffer, &header, sizeof(header));
+    memcpy(buffer+sizeof(header), username, strlen(username));
+
+    // envoi de l'entete
+    int r = sendto(sock, buffer, sizeof(header), 0, (struct sockaddr *) &servadr, len);
+    if (r < 0) {
+        perror("sendto");
+        return -1;
+    }
+
+    // reception reponse serveur
+    memset(buffer, 0, BUF_LEN);
+    r = recvfrom(sock, buffer, BUF_LEN, 0, (struct sockaddr *) &servadr, &len);
+    if (r < 0) {
+        perror("recvfrom");
+        return -1;
+    }
+
+    // recuperation de l'id
+    memcpy(&header, buffer, sizeof(header));
+    id = ntohs(header) >> 5;
+
+    printf("id : %d\n", id);
+
+    // terminaison de la connexion
     close(sock);
 }
--- test/serveur_test.c
+++ test/serveur_test.c
@@ -1,52 +1,70 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <errno.h>
-#include <pthread.h>
+#include <unistd.h>
+#include <ctype.h>
 
 #define SIZE_MESS 100
 #define NUM_THREADS 100
+#define MAX_USERS 100
+#define MAX_USERNAME_LEN 10
 
-struct thread_data {
-    int sockfd;
-    struct sockaddr_storage their_addr;
-    socklen_t addr_len;
-};
+typedef struct {
+    char *pseudo;
+    uint16_t id;
+} utilisateur;
 
-void *serve(void *arg) {
-    struct thread_data *data = (struct thread_data *) arg;
-    int sockfd = data->sockfd;
-    struct sockaddr_storage their_addr = data->their_addr;
-    socklen_t addr_len = data->addr_len;
-    char ipstr[INET6_ADDRSTRLEN];
-    int numbytes;
-    char buf[SIZE_MESS+1];
+typedef struct {
+    int sock;
+    struct sockaddr_in6 addr;
+} client_info;
 
-    printf("Thread started\n");
-    
-    while (1) {
-        if ((numbytes = recvfrom(sockfd, buf, SIZE_MESS+1 , 0, (struct sockaddr *)&their_addr, &addr_len)) == -1) {
-            perror("recvfrom");
-            exit(1);
+int nb_utilisateurs = 0;
+
+utilisateur liste[MAX_USERS];
+client_info clients[MAX_USERS];
+
+int generate_user_id() {
+    static int current_user_id = 0;
+    current_user_id++;
+    current_user_id &= 0x7FF;
+    return current_user_id;
+}
+
+void create_new_user(char *username, int user_id) {
+    utilisateur user;
+    user.pseudo = malloc(sizeof(char*)*MAX_USERS);
+    strcpy(user.pseudo, username);
+    user.id = user_id;
+
+    liste[nb_utilisateurs] = user;
+    nb_utilisateurs++;
+}
+
+void remove_special_chars(char *str) {
+    int i, j;
+    for (i = 0, j = 0; str[i]; i++) {
+        if (!isspace(str[i]) && isprint(str[i])) {
+            str[j++] = str[i];
         }
-
-        printf("got packet from %s\n", inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), ipstr, sizeof ipstr));
-        printf("packet is %d bytes long\n", numbytes);
-        buf[numbytes] = '\0';
-        printf("packet contains \"%s\"\n", buf);
     }
+    str[j] = '\0';
+}
 
-    printf("Thread finished\n");
-    pthread_exit(NULL);
+void completion_pseudo(char *username) {
+    int len = strlen(username);
+    if (len < MAX_USERNAME_LEN) {
+        memset(username+len, '#', MAX_USERNAME_LEN-len);
+        username[MAX_USERNAME_LEN] = '\0';
+        remove_special_chars(username);
+    }
 }
 
 int main(int argc, char *argv[]) {
-    /*int sock = socket(PF_INET6, SOCK_DGRAM, 0);
+    int sock = socket(PF_INET6, SOCK_DGRAM, 0);
     if (sock < 0) return -1;
 
     struct sockaddr_in6 servadr;
@@ -62,62 +80,75 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in6 cliadr;
     socklen_t len = sizeof(cliadr);
 
+    // RECEPTION D'UN CARACTER QUI INDIQUE SI LE CLIENT S'EST DEJA CONNECTE AUPARAVANT AU SERVEUR
+    while (1) {
+        memset(buffer, 0, SIZE_MESS+1);
+        r = recvfrom(sock, buffer, SIZE_MESS, 0, (struct sockaddr *) &cliadr, &len);
+        if (r < 0) return -1;
+        printf("message recu - %d octets : %s\n", r, buffer);
+
+        int connected_before = buffer[0] == '0' ? 0 : 1;
+        printf("connected_before = %d\n", connected_before);
+
+        memset(buffer, 0, SIZE_MESS+1);
+        sprintf(buffer, "%s", connected_before ? "CONNEXION (ENTRE VOTRE IDENTIFIANT)" : "INSCRIPTION (ENTRE UN PSEUDO AVEC MOINS DE 10 CARACTERES)");
+        r = sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr *) &cliadr, len);
+        if (r < 0) return -1;
+
+        // RECEPTION DU PSEUDO OU DE L'ID
+        memset(buffer, 0, SIZE_MESS+1);
+        r = recvfrom(sock, buffer, MAX_USERNAME_LEN, 0, (struct sockaddr *) &cliadr, &len);
+        if (r < 0) return -1;
+
+        if (connected_before == 0) {
+            // INSCRIPTION
+            uint16_t header_client;
+            r = recvfrom(sock, &header_client, sizeof(header_client), 0, (struct sockaddr *) &cliadr, &len);
+            if (r < 0) return -1;
+            uint8_t type = ntohs(header_client) & 0x1F;
+
+            memset(buffer, 0, SIZE_MESS+1);
+            r = recvfrom(sock, buffer, SIZE_MESS, 0, (struct sockaddr *) &cliadr, &len);
+            if (r < 0) return -1;
+
+            completion_pseudo(buffer);
+            int user_id = generate_user_id();
+            create_new_user(buffer, user_id);
+
+            uint16_t header_server = htons((user_id << 5) | (type & 0x1F));
+            r = sendto(sock, &header_server, sizeof(header_server), 0, (struct sockaddr *) &cliadr, len);
+            if (r < 0) return -1;
+        } else {
+            // CONNEXION
+            // TODO
+        }
+    }
+
+    // RECEPTION PAQUET UDP
+    /*uint16_t header_client;
+    r = recvfrom(sock, &header_client, sizeof(header_client), 0, (struct sockaddr *) &cliadr, &len);
+    if (r < 0) return -1;
+    uint8_t type = ntohs(header_client) & 0x1F;
+    uint16_t id = ntohs(header_client) >> 5;
+
     memset(buffer, 0, SIZE_MESS+1);
     r = recvfrom(sock, buffer, SIZE_MESS, 0, (struct sockaddr *) &cliadr, &len);
     if (r < 0) return -1;
-    printf("message recu - %d octets : %s\n", r, buffer);
 
-    memset(buffer, 0, SIZE_MESS+1);
-    sprintf(buffer, "RECU");
-    r = sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr *) &cliadr, len);
-    if (r < 0) return -1;
+    if (type == 1) {
+        // INSCRIPTION
+        int user_id = generate_user_id();
+        create_new_user(buffer, user_id);
+
+        uint16_t header_server = htons((user_id << 5) | (type & 0x1F));
+        r = sendto(sock, &header_server, sizeof(header_server), 0, (struct sockaddr *) &cliadr, len);
+        if (r < 0) return -1;
+    } else if (type == 2) {
+        // CONNEXION
+        // TODO
+    }*/
 
     close(sock);
 
-    return 0;*/
-    struct addrinfo hints, *res, *p;
-    int sockfd;
-    struct sockaddr_storage their_addr;
-    socklen_t addr_len;
-    char ipstr[INET6_ADDRSTRLEN];
-    pthread_t threads[NUM_THREADS];
-    struct thread_data td[NUM_THREADS];
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC; // use IPv4 or IPv6, whichever
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = AI_PASSIVE; // fill in my IP for me
-
-    int rv;
-    if ((rv = getaddrinfo(NULL, argv[1], &hints, &res)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
-    }
-
-    // loop through all the results and bind to the first we can
-    for (p = res; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-            perror("socket");
-            continue;
-        }
-
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("bind");
-            continue;
-        }
-
-        break;
-    }
-
-    if (p == NULL) {
-        fprintf(stderr, "failed to bind socket\n");
-        return 2;
-    }
-
-    freeaddrinfo(res);
-
-    printf("listening on port %s\n", argv[1]);
-
-    for (int i = 0; i < NUM_THREADS; )
+    return 0;
 }
